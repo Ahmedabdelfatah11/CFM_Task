944b67dd3c9d5d8ea12d7537945eaa69ef20f566
Ahmed mohamed <ahmed22mohamed9584@gmail.com>
2025-12-24 01:37:48 +0200

    Final version

--- CFM_Task/Program.cs
+++ CFM_Task/Program.cs
@@ -1,4 +1,9 @@
+using CFM_Task.Models;
+using Microsoft.EntityFrameworkCore;
 
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 namespace CFM_Task
 {
     public class Program
@@ -10,16 +15,29 @@ namespace CFM_Task
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            builder.Services.AddDbContext<CompanyContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
 
             var app = builder.Build();
-
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<CompanyContext>();
+                db.Database.EnsureCreated();
+            }
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
+
+
 
             app.UseHttpsRedirection();
 
